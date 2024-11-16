70c8c6fe51e6dea1bf96ecb97e43b4a5c703029d
GoldenEy3-maker <danil-danil-korolev@bk.ru>
2024-11-15 18:33:53 -0800

    chore: add commented combined tel and email masks

--- src/scripts/tel-mask.ts
+++ src/scripts/tel-mask.ts
@@ -8,6 +8,27 @@ export function initTelMask() {
 
   if (inputs.length)
     inputs.forEach((input) => {
+      // IMask(input, {
+      //   mask: [
+      //     {
+      //       mask: "+{7} (000) 000-00-00",
+      //     },
+      //     {
+      //       mask: "s@d.w",
+      //       blocks: {
+      //         s: { mask: /(?!\d)[a-zA-Z0-9._-]/ },
+      //         d: { mask: /[a-zA-Z._-]/ },
+      //         w: { mask: /[a-zA-Z._-]/ },
+      //       },
+      //     },
+      //   ],
+      //   prepare: (appended, masked) => {
+      //     if (appended === "8" && masked.value === "") {
+      //       return "+7 (";
+      //     }
+      //     return appended;
+      //   },
+      // });
       IMask(input, {
         mask: "+{7} (000) 000-00-00",
         prepare: (appended, masked) => {
